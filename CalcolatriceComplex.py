8e4c57d699998b77d94bea56e269cf49c3f36854
ZenoMartini <47727928+ZenoMartini@users.noreply.github.com>
2019-02-24 23:39:43 +0100

    Add files via upload

--- CalcolatriceComplex.py
+++ CalcolatriceComplex.py
@@ -4,37 +4,23 @@ import math,cmath
 from tkinter import *
 from tkinter import ttk
 
-#Metodi
 #Inserisce risultato operazioni tra Z1 e Z2 in Z3
 def Risultato(c, Z, M, F):
-    #a = round(c.real, 4)
-    #b = round(c.imag, 4)
     #tiene 3 cifre significative della parte reale e di quella immaginaria
-    #'%s': ciò che segue % (operatore di formato) sarà una stringa una stringa
-    #'%.3g' il numero reale che segue % sarà trasformato in stringa che ne
-    #considera 3 cifre significative
+    #'%s': ciò che segue % (operatore di formato) sarà una stringa
+    #'%.4g' il numero reale che segue % sarà trasformato in stringa che ne
+    #considera 4 cifre significative
     #la stringa prodotta deve essere convertita in numero reale
-    a = float('%s' % float('%.3g' % c.real))
-    b = float('%s' % float('%.3g' % c.imag))
+    a = float('%s' % float('%.4g' % c.real))
+    b = float('%s' % float('%.4g' % c.imag))
     c = a + b * 1j
     Z.delete(0, END)
     Z.insert(0, c)
     RtoP(Z, M, F)
     Messaggi.delete(0, END)
-    Impedenze=(Z1.get(),Z2.get(),Z3.get(),Z4.get())
+    CancellaGrafici()
+    DisegnaGrafici()
 
-
-#Somma Z1 con Z2
-def SommaZ1Z2():
-    try:
-        a=Z1.get()
-        b=Z2.get()
-        c=complex(a)+complex(b)
-        Risultato(c, Zr, Mr, Fr)
-        print(z1var.get())
-    except ValueError:
-        Messaggi.delete(0, END)
-        Messaggi.insert(0, 'Scrittura errata del numero')
 #Sottrai Z2 da Z1
 def SottrazioneZ1Z2():
     try:
@@ -46,17 +32,35 @@ def SottrazioneZ1Z2():
         Messaggi.delete(0, END)
         Messaggi.insert(0, 'Scrittura errata del numero')
 
-#moltiplica Z1 con Z2
-def MoltiplicazioneZ1Z2():
-    try:
-        a=Z1.get()
-        b=Z2.get()
-        c=complex(a)*complex(b)
-        Risultato(c, Zr, Mr, Fr)
-    except ValueError:
-        Messaggi.delete(0, END)
-        Messaggi.insert(0, 'Scrittura errata del numero')
 #dividi Z1 con Z2
+def addizione():
+    a=0
+    c=complex(a)
+    for i in range(len(zvar)):
+        if zvar[i].get():
+            try:
+                a=Impedenze[i].get()
+                c =c+ complex(a)
+            except ValueError:
+                pass
+    Risultato(c, Zr, Mr, Fr)
+
+def moltiplicazione():
+    a = 1
+    c = complex(a)
+    for i in range(len(zvar)):
+        if zvar[i].get():
+            try:
+                a = Impedenze[i].get()
+                c = c * complex(a)
+            except ValueError:
+                pass
+    Risultato(c, Zr, Mr, Fr)
+
+def sottrazione():
+    pass
+def divisione():
+    pass
 def DivisioneZ1Z2():
     try:
         a=Z1.get()
@@ -77,11 +81,10 @@ def DivisioneZ1Z2():
 
 def ZParalleli(n,Impedenze):
     y=0
-    for i in range(0, n):
-        #print(i)
-        #print(complex(Impedenze[i].get()))
+    for i in range(n):
         try:
             x=complex(Impedenze[i].get())
+            print("Zparalleli",x)
             if x==0:
                 Zr.delete(0, END)
                 Zr.insert(0, '0')
@@ -89,7 +92,6 @@ def ZParalleli(n,Impedenze):
             else:
                 try:
                     y=1/x+y
-                    #print(x)
                 except ZeroDivisionError:
                     Zr.delete(0,END)
                     Zr.insert(0,'infinito')
@@ -102,9 +104,32 @@ def ZParalleli(n,Impedenze):
         try:
             z = 1 / y
             Risultato(z, Zr, Mr, Fr)
+
         except ZeroDivisionError:
             Zr.delete(0, END)
             Zr.insert(0, "infinito")
+
+def Paralleli():
+    zp=[]
+    i=0
+    if z1var.get():
+        zp.append(Z1)
+        i=i+1
+    if z2var.get():
+        zp.append(Z2)
+        i = i + 1
+    if z3var.get():
+        zp.append(Z3)
+        i = i + 1
+    if z4var.get():
+        zp.append(Z4)
+        i = i + 1
+    if i>1:
+        ZParalleli(i,zp)
+        del zp[:]
+    else:
+        Messaggi.delete(0, END)
+        Messaggi.insert(0, "Occorre selezionare almeno due Z")
 
 #trasforma in coordinare cartesiane
 def PtoRs():
@@ -152,11 +177,14 @@ def RtoP(Z, M, F):
         Messaggi.insert(0, 'Scrittura errata del numero')
 
 #copia Zr in una delle Z
-def ZrtoZ(Z,M,F):
-    Z.delete(0,END)
-    Z.insert(0,Zr.get())
-    RtoP(Z,M,F)
-
+def ZrtoZ(Z,M,F,Mem):
+    if z4var.get():
+        Memoria(Z, M, F, Mem)
+        Z.delete(0,END)
+        Z.insert(0,Zr.get())
+    else:
+        Messaggi.delete(0, END)
+        Messaggi.insert(0, 'Z4 non è selezionata')
 
 def ScambioZSpuntate():
     if z1var.get():
@@ -221,18 +249,16 @@ def Memoria(Z,M,F,Mem):
     a=Z.get()
     b=Mem.get()
     if a !='':
-        #Z.delete(0,END)
-        #Z.insert(0,Mem.get())
         Mem.delete(0,END)
         Mem.insert(0,a)
         if b != '':
             Z.delete(0,END)
             Z.insert(0,b)
-
-    #print(Mem.get(),Z.get())
+            RtoP(Z,M,F)
 
 #calcola l'inverso
 def invZS():
+
     if z1var.get():
         invZ(Z1, M1, F1)
     if z2var.get():
@@ -242,20 +268,19 @@ def invZS():
     if z4var.get():
         invZ(Z4, M4, F4)
 
-
 #calcola l'inverso
 def invZ(Z, M, F):
+    Messaggi.delete(0, END)
     try:
         a=complex(Z.get())
         if a !=0:
             a=1/a
             Risultato(a, Z, M, F)
         else:
-            Messaggi.delete(0, END)
             Messaggi.insert(0, 'Non si può dividere per zero')
     except ValueError:
-        Messaggi.delete(0, END)
-        Messaggi.insert(0, 'Scrittura errata del numero')
+        #Messaggi.delete(0, END)
+        Messaggi.insert(0, 'Scrittura errata Z')
 def PiuMenoZs():
     if z1var.get():
         PiuMeno(Z1, M1, F1)
@@ -279,6 +304,17 @@ def ResetBottone():
     for i in range(19):
         Campi[i].delete(0,END)
     Messaggi.delete(0, END)
+def SetZ():
+    ResetBottone()
+    Z1.delete(0, END)
+    Z1.insert(0, "3+4j")
+    Z2.delete(0, END)
+    Z2.insert(0, "-3+3j")
+    Z3.delete(0, END)
+    Z3.insert(0, "-2+4j")
+    Z4.delete(0, END)
+    Z4.insert(0, "-4-3j")
+
 def YtoD():
     try:
         a=complex(Z1.get())
@@ -288,7 +324,7 @@ def YtoD():
             Messaggi.delete(0, END)
             Messaggi.insert(0, 'Non è una stella:qualche impedenza è nulla')
         else:
-            Memoria(Z1,M1,F1,Mem1)
+            Memoria(Z1, M1, F1, Mem1)
             Memoria(Z2, M2, F2, Mem2)
             Memoria(Z3, M3, F3, Mem3)
             N=a * b + b * c + a * c
@@ -315,12 +351,12 @@ def DtoY():
             Memoria(Z1, M1, F1, Mem1)
             Memoria(Z2, M2, F2, Mem2)
             Memoria(Z3, M3, F3, Mem3)
-            D=a+b+c
-            c1=a*c/D
+            D = a+b+c
+            c1 = a*c/D
             Risultato(c1, Z1, M1, F1)
-            c1=b*a/D
+            c1 = b*a/D
             Risultato(c1, Z2, M2, F2)
-            c1=b*c/D
+            c1 = b*c/D
             Risultato(c1, Z3, M3, F3)
 
     except ValueError:
@@ -337,27 +373,46 @@ def SpostamentoCentroStella():
             Messaggi.delete(0, END)
             Messaggi.insert(0, 'Spostamento infinito o indefinito')
         else:
-            f2=cmath.rect(1, -2 * math.pi / 3)
-            f3=cmath.rect(1, 2 * math.pi / 3)
-            #print(f2,f3)
-
             V = d*(1 / a + f2 / b + f3 / c) / ( 1 / a + 1 / b + 1 / c)
             Risultato(V, Zr, Mr, Fr)
    except ValueError:
        Messaggi.delete(0, END)
        Messaggi.insert(0, "C'è almeno una scrittura errata nei dati")
 
+def Sequenze():
+    try:
+        a = complex(Z1.get())
+        b = complex(Z2.get())
+        c = complex(Z3.get())
+    #diretta
+        diretta=(a+b*f3+c*f2)/3
+        Memoria(Z1, M1, F1, Mem1)
+        Risultato(diretta, Z1, M1, F1)
+    #inversa
+        inversa = (a + b * f2 + c*f3)/ 3
+        Memoria(Z2, M2, F2, Mem2)
+        Risultato(inversa, Z2, M2, F2)
+    #omopolare
+        zero=(a+b+c)/3
+        Memoria(Z3,M3,F3,Mem3)
+        Risultato(zero,Z3,M3,F3)
+
+    except ValueError:
+        Messaggi.delete(0, END)
+        Messaggi.insert(0, "C'è almeno una scrittura errata nei dati")
 
 def DisegnaGrafici():
     # coordinate relative grafico
-    try:
-        z1=cmath.polar(complex(Z1.get()))
-        z2=cmath.polar(complex(Z2.get()))
-        z3=cmath.polar(complex(Z3.get()))
-        z4=cmath.polar(complex(Z4.get()))
-        zr=cmath.polar(complex(Z4.get()))
 
-        L= max(z1[0],z2[0],z3[0],z4[0],zr[0])
+    try:
+        zi = []
+        for i in range(len(Impedenze)):
+            c = cmath.polar(complex(Impedenze[i].get()))
+            zi.append(c)
+
+        L= max(zi[0][0],zi[1][0],zi[2][0],zi[3][0],zi[4][0])
+
+        del zi[:]
 
         xmin = -L
         xmax = L
@@ -366,22 +421,26 @@ def DisegnaGrafici():
 
         k1=LargTela/(xmax-xmin) # calcolo costanti di conversione
         k2=AltTela/(ymax-ymin)
+        s=L/5 #scala
 
         PZn=[]
         for i in range(len(Impedenze)):
             PZ = (k1 * complex(Impedenze[i].get()).real + Xv0, Yv0 - k2 * complex(Impedenze[i].get()).imag)
             PZn.append(PZ)
 
-        r=5
+        r=4
 
         for i in range(len(PZn)):
             cz = canv.create_oval(PZn[i][0] + r, PZn[i][1] - r, PZn[i][0] - r, PZn[i][1] + r, fill=Zcolor[i])
             Pdisegnati.append(cz)
+        um=canv.create_text(25,40,text="1:"+str(round(s,1 )))
+        Pdisegnati.append(um)
         del PZn[:]
 
     except ValueError:
+        Impedenze[i].delete(0,END)
         Messaggi.delete(0, END)
-        Messaggi.insert(0, 'Scrittura errata di qualche impedenza')
+        Messaggi.insert(0, 'Scrittura errata Z'+str(i+1))
 
 def CancellaGrafici():
     #print(len(Pdisegnati))
@@ -389,46 +448,70 @@ def CancellaGrafici():
         canv.delete(Pdisegnati[i])
     del Pdisegnati[:]
 
+#costanti varie
+
+#operatori di sequenza
+f2=cmath.rect(1, -2 * math.pi / 3) #2
+f3=cmath.rect(1, 2 * math.pi / 3) #3
+
+# margini esterni del widget
+sinistra=10
+sopra=10
+destra=10
+sotto=10
+impacchettamento=(sinistra,sopra,destra,sotto)
+#aspetto del frame
+rilievo=("flat", "raised", "sunken", "groove", "ridge")
+#dimensioni widget
+LEZ=20 #Larghezza Entry campi Z
+LEM=8 #Larghezza Entry campi M ed F
+FEZ="Baskerville 24"#Font Entry campi Z
+FEM="Arial 14"#Font Entry campi M ed F
+#parametro posizione widget
+cz=3 #colonna delle z
+rb=6 #start colonna bottoni operazioni
+
+
+# riquadro grafico e punto centrale
+LargTela=500
+AltTela=500
+Xv0 = LargTela / 2
+Yv0 = AltTela / 2
+# parametri griglia ed assi
+stepx = LargTela/10 #distanza tra linee verticali
+stepy = AltTela/10 #distanza tra linee orizzontali
+nx = int(Xv0 / 50) + 1
+ny = int(Yv0 / 50) + 1
+#colori usati
+Zcolor=('firebrick','dodgerblue','darkgreen','goldenrod','red')
+
 #finestra principale
 
 root = Tk()
 root.title("Calcolatrice per numeri complessi")
 root.configure(background="green")
 
-sinistra=10
-sopra=10
-destra=10
-sotto=10
-impacchettamento=(sinistra,sopra,destra,sotto)
-rilievo=("flat", "raised", "sunken", "groove", "ridge")
-#genera il widget "frame principale" di nome content
-content = ttk.Frame(root, padding=impacchettamento,relief="sunken")
-content1 = ttk.Frame(root, padding=impacchettamento,relief="sunken")
-LargTela=500
-AltTela=500
-Xv0 = LargTela / 2
-Yv0 = AltTela / 2
+
+#genera due widget "frame" dentro la finestra principale
+content = ttk.Frame(root, padding=impacchettamento,relief="sunken") #contiene tasti e campi
+
+content1 = ttk.Frame(root, padding=impacchettamento,relief="sunken") #contiene il grafico
+#genera la tela per disegnare
 canv = tk.Canvas(content1, highlightthickness=0, bg='white', width=LargTela, height=AltTela)
 #canv1 = tk.Canvas(content1, highlightthickness=0, bg='lightgreen', width=LargTela, height=AltTela, relief="sunken")
 
 
 #definizioni etichette
-z1lbl=ttk.Label(content, text="Z1", font="Helvetica 24")
-z2lbl=ttk.Label(content, text="Z2", font="Helvetica 24")
-z3lbl=ttk.Label(content, text="Z3", font="Helvetica 24")
-z4lbl=ttk.Label(content, text="Z4", font="Helvetica 24")
-zrlbl=ttk.Label(content, text="Zr", foreground="red", font="Helvetica 24",justify=CENTER)
+
+zrlbl=ttk.Label(content, text="Zr", foreground="red", font="Helvetica 20",justify=CENTER)
 
 cartesianalbl=ttk.Label(content, text="a+bj",font="Helvetica 16")
 Modulolbl=ttk.Label(content, text="Modulo",font="Helvetica 16")
 Faselbl=ttk.Label(content, text="Fase in gradi",font="Helvetica 16")
 
 #definizione campi di ingresso
-LEZ=15 #Larghezza Entry Z
-LEM=8 #Larghezza Entry M ed F
-FEZ="Baskerville 24"#Font Entry Z, M, F
-FEM="Arial 14"
-Zcolor=('firebrick','royalblue','seagreen','goldenrod','red')
+
+
 c1=ttk.Style()#definisce l'aspetto del campo come colore del fomdo, del carattere
 c1.configure('Z1.TEntry',foreground=Zcolor[0],background="whitesmoke")
 c2=ttk.Style()#definisce l'aspetto del campo come colore del fomdo, del carattere
@@ -479,19 +562,18 @@ s = ttk.Style()
 s.configure('my.TButton', font=('Helvetica', 18), foreground="blue")
 
 #definizione bottoni
-Somma = ttk.Button(content, text="Z1+Z2", style="my.TButton", command=SommaZ1Z2)
+Somma = ttk.Button(content, text="addizione", style="my.TButton", command=addizione)
 Sottrazione = ttk.Button(content, text="Z1-Z2",style="my.TButton", command=SottrazioneZ1Z2)
-Moltiplicazione = ttk.Button(content, text="Z1xZ2", style="my.TButton", command=MoltiplicazioneZ1Z2)
+Moltiplicazione = ttk.Button(content, text="moltiplicazione", style="my.TButton", command=moltiplicazione)
 Divisione = ttk.Button(content, text="Z1/Z2", style="my.TButton", command=DivisioneZ1Z2)
-Parallelo = ttk.Button(content, text="Z1||Z2", style="my.TButton", command=lambda:ZParalleli(2, Impedenze))
-Parallelo3 = ttk.Button(content, text="Z1||Z2||Z3", style="my.TButton", command=lambda:ZParalleli(3, Impedenze))
-Parallelo4 = ttk.Button(content, text="Z1||Z2||Z3||Z4", style="my.TButton", command=lambda:ZParalleli(4, Impedenze))
+
+ParalleliButton = ttk.Button(content, text="Paralleli", style="my.TButton", command=Paralleli)
 
 PtoRZs = ttk.Button(content, text="P->R", style="my.TButton", command=PtoRs)
 
 RtoPZs = ttk.Button(content, text="R->P", style="my.TButton", command=RtoPs)
 
-ZrtoZ4 = ttk.Button(content, text="Zr->Z4", style="my.TButton", command=lambda:ZrtoZ(Z4, M4, F4))
+ZrtoZ4 = ttk.Button(content, text="Zr->Z4", style="my.TButton", command=lambda:ZrtoZ(Z4, M4, F4, Mem4))
 
 z1var=BooleanVar()
 z2var=BooleanVar()
@@ -503,10 +585,11 @@ z2 = ttk.Checkbutton(content, text="Z2", variable=z2var, onvalue=True)
 z3 = ttk.Checkbutton(content, text="Z3", variable=z3var, onvalue=True)
 z4 = ttk.Checkbutton(content, text="Z4", variable=z4var, onvalue=True)
 
-z1var.set(False)
-z2var.set(False)
-z3var.set(False)
+z1var.set(True)
+z2var.set(True)
+z3var.set(True)
 z4var.set(False)
+zvar=(z1var,z2var,z3var,z4var)
 
 ScambioZS = ttk.Button(content, text="Scambio Z", style="my.TButton", command=ScambioZSpuntate)
 
@@ -519,10 +602,13 @@ ZsMem=ttk.Button(content, text="M<->Z", style="my.TButton", command=MemZs)
 YD=ttk.Button(content, text="YD123", style="my.TButton", command=YtoD)
 DY=ttk.Button(content, text="DY123", style="my.TButton", command=DtoY)
 SCS=ttk.Button(content, text="SCY:Z4=U", style="my.TButton", command=SpostamentoCentroStella)
+SequenzeBottone=ttk.Button(content,text="Sequenze",style="my.TButton",command=Sequenze)
 
 Reset = ttk.Button(content, text="Reset", style="my.TButton", command=ResetBottone)
 Disegna = ttk.Button(content, text="Disegna", style="my.TButton", command=DisegnaGrafici)
 Cancella = ttk.Button(content, text="Cancella", style="my.TButton", command=CancellaGrafici)
+
+SetZButton = ttk.Button(content, text="Set", style="my.TButton", command=SetZ)
 
 
 
@@ -530,14 +616,13 @@ Cancella = ttk.Button(content, text="Cancella", style="my.TButton", command=Canc
 content.grid(column=0, row=0, sticky=(N, S, E, W))
 content1.grid(column=0, row=1, sticky=(N, S, E, W))
 canv.grid(column=1, row=1, sticky=(E))
-#canv1.grid(column=7, row=1, sticky=(W))
 
 #posizionamento etichette
-cz=3 #colonna delle z
-rb=6 #start colonna bottoni operazioni
+
 cartesianalbl.grid(column=cz+2, row=0)
 Modulolbl.grid(column=cz+3, row=0)
 Faselbl.grid(column=cz+4, row=0)
+zrlbl.grid(column=cz+1,row=5)
 
 z1.grid(column=cz+1, row=1)
 z2.grid(column=cz+1, row=2)
@@ -563,8 +648,7 @@ F3.grid(column=cz+4, row=3, sticky=(W))
 F4.grid(column=cz+4, row=4, sticky=(W))
 Fr.grid(column=cz+4, row=5, sticky=(W))
 
-#Messaggi.grid(column=cz+1, row=rb+2, columnspan=4, sticky=(W,E))#,column=cz-3)#, columnspan=8)#column=cz+1,
-Messaggi.grid(column=0, row=0, columnspan=10, sticky=(W,E))#,column=cz-3)#, columnspan=8)#column=cz+1,
+Messaggi.grid(column=0, row=0, columnspan=10, sticky=(W,E))
 
 
 #posizionamento bottoni
@@ -572,12 +656,13 @@ Somma.grid(column=cz-3, row=rb+1, ipadx=5)
 Sottrazione.grid(column=cz-2, row=rb+1, ipadx=5)
 Moltiplicazione.grid(column=cz-1, row=rb+1, ipadx=5)
 Divisione.grid(column=cz, row=rb+1, ipadx=5)
-Parallelo.grid(column=cz-3, row=rb+2, ipadx=5)
-Parallelo3.grid(column=cz-2, row=rb+2, ipadx=5)
-Parallelo4.grid(column=cz-1, row=rb+2, ipadx=5)
+ParalleliButton.grid(column=cz, row=rb+2, ipadx=5)
+
 YD.grid(column=cz-3,row=rb+3, ipadx=5)
 DY.grid(column=cz-2,row=rb+3, ipadx=5)
 SCS.grid(column=cz-1,row=rb+3, ipadx=5)
+SequenzeBottone.grid(column=cz,row=rb+3, ipadx=5)
+
 
 PtoRZs.grid(column=2, row=2)
 
@@ -594,16 +679,13 @@ ZsPiuMeno.grid(column=cz, row=rb-5)
 ZsMem.grid(column=cz, row=rb-2, ipadx=5)
 
 Reset.grid(column=0, row=0, ipadx=10)
+SetZButton.grid(column=0, row=1, ipadx=10)
+
 Disegna.grid(column=1, row=0, ipadx=10)
 Cancella.grid(column=2, row=0, ipadx=10)
 
-# funzioni di conversione
+    # Tracciamento griglia ed assi
 
-    # Griglia ed assi
-stepx = LargTela/10
-stepy = AltTela/10
-nx = int(Xv0 / 50) + 1
-ny = int(Yv0 / 50) + 1
 Griglia=[]
 for i in range(-nx, nx):
     grix = canv.create_line(i * stepx + Xv0, 0, i * stepx + Xv0, AltTela, fill="lightgrey")
